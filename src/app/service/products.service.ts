1c50298be707fa1a236fe8892d5d57f6e328101e
LOX91 <109281442+LOX91@users.noreply.github.com>
2023-11-01 22:38:17 +0100

    fin du crud mais useguars desactivé dans back probleme token

--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -9,39 +9,49 @@ import { Observable } from 'rxjs';
 export class ProductsService {
   private bddUrl = 'http://localhost:3000/api/produit';
 
-  constructor(private Http: HttpClient) {}
-  setHeader(){
-
-    const jwtToken=localStorage.getItem('token');
-    const headers=new HttpHeaders({
-
-
-    })
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('access_token');
+    let headers = new HttpHeaders();
+    if (token) {
+      headers = headers.set('Authorization', 'Bearer ' + token);
+    }
+    return headers;
   }
 
-
-
+  constructor(private Http: HttpClient) {}
 
   getProducts(): Observable<Products[]> {
     return this.Http.get<Products[]>('http://localhost:3000/api/produit');
   }
 
-  getProductById(id: number):Observable<Products> {
+  getProductById(id: number): Observable<Products> {
     return this.Http.get<Products>(`${this.bddUrl}/${id}`);
   }
 
-  addProduct(product: Products) {
-    return this.Http.post<Products>(this.bddUrl, product,);
-  }
-  updateProduct(id:number,product: Products)
-   {console.log("remy est gentil",id,product);
-    return this.Http.patch<Products>(`${this.bddUrl}/${id}`, product);
+  addProduct(product: Products):Observable<Products>{
+    const headers = new HttpHeaders({
+      Authorisation: 'Bearer ' + localStorage.getItem('access_token'),
+    });
+    return this.Http.post<Products>(
+    'http://localhost:3000/api/produit', product,{ headers: headers },
+    );
+
+    }
+  updateProduct(id: number, product: Products):Observable<Products> {
+
+
+    return this.Http.patch<Products>(`http://localhost:3000/api/produit/${id}`, product,{ headers: this.getHeaders() }
+    );
   }
   removeProduct(id: number) {
-    return this.Http.delete(`${this.bddUrl}/${id}`);
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+    });
+    return this.Http.delete(`http://localhost:3000/api/produit/${id}`,{
+      headers:headers,
+    });
   }
 }
 function newHttpHeaders(arg0: {}) {
   throw new Error('Function not implemented.');
 }
-
